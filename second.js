7b719246dd99f20842eb4063fe85a76449d04a39
ranasaqib678 <146761624+ranasaqib678@users.noreply.github.com>
2023-11-21 13:21:20 +0500

    Add files via upload

---
+++ second.js
@@ -0,0 +1,10 @@
+Math.random;
+var decimal =Math.random();
+console.log(decimal);
+console.log(decimal*10);
+var round =Math.trunc(decimal*10);
+console.log(round);
+"ho-ho-ho".split("-");
+var greet ="hello";
+greet.toUpperCase();
+greet.toLowerCase();
